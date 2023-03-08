360bf6eb031995c05cc47eb3d6fb2051a3f5f466
Antonio Contreras <antoniocontreras71@yahoo.com>
2023-03-08 14:27:42 -0600

    stuck on calculator

--- calculator.js
+++ calculator.js
@@ -1,16 +1,20 @@
-let allButtons = document.querySelectorAll('.grid-item');
+//let allButtons = document.querySelectorAll('.grid-item');
+//let storeNumbers = [];
+//2 + 3 * 4 / 5 -6 + 7
 let userInputs = document.getElementById('#userInput');
 let showAnswer = document.getElementById('#showAnswer');
 let equalsButton = document.getElementById('#equalsBtn');
 let inputs = document.querySelectorAll('.number, .operand');
 let storeUserInput = [];
-let storeNumbers = [];
+let chosenNum;
 for (input of inputs) {
     input.addEventListener('click', function (e) {
-            storeUserInput.push(input.value);
-            userInput.textContent += e.target.value;
+        storeUserInput.push(userInput.textContent += e.target.value);
+        console.log(e.target.value + ' is num1');
+        //showAnswer.textContent = e.target.value;;
     });
 }
+//equalsButton.addEventListener('click', operate);
 function add() {
 
 }
@@ -21,5 +25,10 @@ function multiply() {
 
 }
 function divide() {
-    
+
 }
+function operate(operator, num1, num2) {
+    //return parseInt(num1) + operator + parseInt(num2);
+    alert('hi');
+}
+let nestedArray = [['deep'],[['deeper'], ['deep'] ],[[['deeper'], ['deepest']],[[['deepest']]]]];
